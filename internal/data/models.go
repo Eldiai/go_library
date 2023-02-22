bfa2b811b41a1975a3c1e6b66d42c48104fbc899
Elnara <kimelim85@gmail.com>
2023-02-22 22:43:42 +0600

    CRUD book,corrected migration, some models

---
+++ internal/data/models.go
@@ -0,0 +1,31 @@
+package data
+
+import (
+	"database/sql"
+	"errors"
+)
+
+var (
+	ErrRecordNotFound = errors.New("record not found")
+	ErrEditConflict   = errors.New("edit conflict")
+)
+
+type Models struct {
+	Books interface {
+		Insert(book *Book) error
+		Get(id int64) (*Book, error)
+		Update(book *Book) error
+		Delete(id int64) error
+		//GetAll(title string, genres []string, filters Filters) ([]*Movie, Metadata, error)
+	}
+	//Users  UserModel
+	//Tokens TokenModel
+}
+
+func NewModels(db *sql.DB) Models {
+	return Models{
+		Books: BookModel{DB: db},
+		//Tokens: TokenModel{DB: db},
+		//Users:  UserModel{DB: db},
+	}
+}
